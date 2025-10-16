2bbb36a8ae8175da9d9bf5e270a46a8b198e79aa
singhannu7787-ux <singhannu7787@gmail.com>
2025-10-16 14:11:55 +0530

    Update urls.py

--- connectly/urls.py
+++ connectly/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('social.urls')),
+    path('', include('social.urls')),  # connects to your app's urls.py
+
+    # Authentication routes
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
