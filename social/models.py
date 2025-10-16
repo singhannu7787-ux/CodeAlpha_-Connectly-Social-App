478708b1c07faaf2270e0684be60af14c43cdba8
singhannu7787-ux <singhannu7787@gmail.com>
2025-10-16 14:15:05 +0530

    Update models.py

--- social/models.py
+++ social/models.py
@@ -1,26 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# -----------------------------------------------------------
+# User Profile Model
+# -----------------------------------------------------------
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    profile_pic = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', default='default.png')
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
 
+    def __str__(self):
+        return self.user.username
+
+# -----------------------------------------------------------
+# Post Model
+# -----------------------------------------------------------
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    caption = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.caption[:30]}'
+
+    @property
+    def total_likes(self):
+        return self.likes.count()
+
+# -----------------------------------------------------------
+# Comment Model
+# -----------------------------------------------------------
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class Follow(models.Model):
-    follower = models.ForeignKey(User, related_name='follower', on_delete=models.CASCADE)
-    following = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.user.username} commented: {self.text[:30]}'
